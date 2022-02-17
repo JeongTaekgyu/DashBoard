a0edcee1bc2102129178ee26a8d387161bcd7bc8
taekgyu <taekgyu0602@gmail.com>
2022-02-18 02:38:48 +0900

    main 에서 getter 추가

--- src/main/java/com/example/dashboard/model/User.java
+++ src/main/java/com/example/dashboard/model/User.java
@@ -1,22 +1,18 @@
 package com.example.dashboard.model;
 
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Getter
 @Table(name = "user")
 public class User {
     @Id
     @Column(name = "id", nullable = false)
     private Long id;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
